219ccd55f98154f7b06ffda2a4a20758bc9f6954
Jan <janro06@micorreo.upp.edu.mx>
2022-02-25 11:25:09 -0600

    Cambio de informacion para conexion con Mysql

--- src/Modelo/MDB.java
+++ src/Modelo/MDB.java
@@ -17,7 +17,7 @@ public class MDB {
         //conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://127.0.0.1:3306/bddatos", "root","administrador");
 	//public Conexion(String controlador, String contrasena, String URL, String usuario) {
 	//	conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://localhost:3306/bdcostos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&useTimezone=true&serverTimezone=GMT&useSSL=false", "root","administrador");
-        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5473775", "sql5473775","ZCmVlEZdp7");
+        conexion = new Conexion("com.mysql.jdbc.Driver",  "jdbc:mysql://sql3.freesqldatabase.com:3306/sql3475350", "sql3475350","lGK1BHRzFf");
 
         }
 
